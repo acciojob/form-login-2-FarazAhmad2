8c84fd2c925153c6eb565304248039922cb05cff
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-07 17:36:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// let btn = document.getElementById('btn');
+
+function myFunc() {
+  let fname = document.getElementById('fname');
+  let lname = document.getElementById('lname');
+  let email = document.getElementById('email');
+  let phone = document.getElementById('phone');
+  alert('First Name: '+ fname.value + ' ' + 'Last Name: '+ lname.value + 'Phone Number: ' + phone.value + ' ' +  "Email ID: " +email.value);
+}
